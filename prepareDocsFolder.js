3c92e9bee628e307609733061bb7e93b96504340
Panagiotis Riccardo Siavelis <prsiavelis@gmail.com>
2022-05-22 22:58:33 +0300

    Publishing to gh-pages

--- prepareDocsFolder.js
+++ prepareDocsFolder.js
@@ -1,40 +1,26 @@
 const fs = require("fs-extra");
 
-const removeFolder = (dir) => fs.rm(dir, {recursive: true}, (err) => {
-    if (err) {
-        throw err;
-    }
-
-    console.log(`${dir} is deleted!`);
-});
-//
-// fs.unlinkSync("production/.git", (err) => {
-//     if (err) {
-//         throw err;
-//     }
-//
-//     console.log(`production/.git is deleted!`);
-// });
-
-// copy worktree related information to production folder
-fs.copySync("docs/.git", "production/.git",);
-
 // add .nojekyll
 fs.writeFile("production/.nojekyll", "", 'utf8', (err) => {
     if (err) {
-        console.log(err);
-        throw err;
+        console.error(err);
     }
 });
 
+const removePath = (path) => fs.rm(path, {recursive: true}, (err) => {
+    if (!err) {
+        console.log(`${path} is deleted!`);
+    }
+
+});
+
 // remove old "docs" folder
-removeFolder("docs");
+removePath("docs/_next");
 
 // copy "production" -> "docs"
 try {
-    fs.copySync("production/", "docs/")
+    fs.copySync("production", "docs")
     console.log('success!')
 } catch (err) {
     console.error(err);
-    throw err;
 }
