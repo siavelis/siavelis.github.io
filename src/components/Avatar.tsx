1a8627f094557ba1cfbd2bd0de5cdd9ccea9fa40
Panagiotis Riccardo Siavelis <prsiavelis@gmail.com>
2022-07-12 22:39:58 +0300

    Update bio based on JSON Resume from LinkedIn account

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,16 +1,14 @@
-import {Col, Image} from 'react-bootstrap';
+import {Image} from 'react-bootstrap';
 import React from 'react';
 
 function Avatar() {
-    return <Col
-        xs={12} sm={6} md={4} lg={{offset: 2, span: 3}}>
-        <Image
-            src='/photo.jpeg'
-            rounded
-            className='img-fluid '
-            alt="Website owner's photo."
-        />
-    </Col>
+    return <Image
+        alt="Website owner's photo."
+        src='/photo.jpeg'
+        roundedCircle
+        thumbnail
+        style={{width: '15vw'}}
+    />
 }
 
 export default Avatar;
