11bbdc46b91655d832da94bd0a73ae2ee4206c2e
Panagiotis Riccardo Siavelis <prsiavelis@gmail.com>
2022-07-12 19:11:07 +0300

    Provision index.css for global styling

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import {AppProps} from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
+import '../public/css/index.css';
 
 function App({Component, pageProps}: AppProps) {
     return <Component {...pageProps} />
