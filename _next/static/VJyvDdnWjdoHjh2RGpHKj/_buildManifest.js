5d3acc4388290631c1dcfa8404b8f69fe5b28136
Panagiotis Riccardo Siavelis <prsiavelis@gmail.com>
2022-07-12 22:59:44 +0300

    Publishing to gh-pages

--- _next/static/VJyvDdnWjdoHjh2RGpHKj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/36-111a2a270e1076a5.js","static/chunks/pages/index-199f5e5458ef475a.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
